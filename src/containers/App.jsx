b0623d9c619ff1294d03ebad665971e49d63c221
pabloPc <pjorda96@gmail.com>
2020-07-05 13:52:50 +0200

    structure

---
+++ src/containers/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { HashRouter, Switch } from 'react-router-dom';
+import '../scss/App.scss';
+
+// const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+
+// Containers
+// const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout/DefaultLayout'));
+//
+// Pages
+// const Login = React.lazy(() => import('./views/Pages/Login/'));
+// const Register = React.lazy(() => import('./views/Pages/Register/'));
+// const Page404 = React.lazy(() => import('./views/Pages/Page404/'));
+// const Page500 = React.lazy(() => import('./views/Pages/Page500/'));
+
+function App() {
+  return (
+    <HashRouter>
+      <React.Suspense > {/*fallback={loading()}*/}
+        <Switch>
+          {/*<Route exact path="/login" render={props => <Login id={1} {...props}/>} />
+          <Route exact path="/register" render={props => <Register {...props}/>} />
+          <Route exact path="/404" render={props => <Page404 {...props}/>} />
+          <Route exact path="/500" render={props => <Page500 {...props}/>} />
+          <Route path="/" render={props => <DefaultLayout {...props}/>} />*/}
+          <p>Hello World</p>
+        </Switch>
+      </React.Suspense>
+    </HashRouter>
+  );
+}
+
+export default App;
